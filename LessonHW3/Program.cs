3b602f704e420db4bfd8175695062ef9fd5df99f
Dell <Dell@DESKTOP-B6FMJUN>
2022-03-17 14:43:14 +0200

    Добавьте файлы проекта.

---
+++ LessonHW3/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace LessonHW3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Запросить у пользователя три целочисленных значения и найти максимальное.
+
+            Console.WriteLine("Введите 3 целых числа:");
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            int c = int.Parse(Console.ReadLine());
+
+            if (a > b && a > c)
+            {
+                Console.WriteLine($"Максиальное число {a} ");
+            }
+
+            else if (b > a && b > c)
+            {
+                Console.WriteLine($"Максиальное число {b} ");
+            }
+
+            else if (c > b && c > a)
+            {
+                Console.WriteLine($"Максиальное число {c} ");
+            }
+                    
+            Console.ReadLine();
+        }
+    }
+}
